5775d2f9276b0c8a5e42e813110b81bd455124d4
owen32497 <32204846+owen32497@users.noreply.github.com>
2019-02-13 01:07:41 -0500

    partially done with get

--- http_sockets.py
+++ http_sockets.py
@@ -1,26 +1,41 @@
-#!/usr/bin/python3
 import socket
-from urllib.parse import urlparse
-from urllib.parse import urlencode
+from urlparse import urlparse
+import urllib
 class sock:
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def __init__(self, host, port):
+        self.HOST = host
+        self.PORT = port
         self.s.connect((host, port))
     
     def post(self, url, params={}):
         url_parsed = urlparse(url)
-        params_formatted = urlencode(params)
+        params_formatted = urllib.urlencode(params)
 
-        header = bytes('POST {} HTTP/1.1\r\n'.format(url_parsed.path))
-        header += bytes(b'Host: {}\r\n'.format(url_parsed.netloc))
-        header += bytes('Content-Type: application/x-www-form-urlencoded\r\n')
-        header += bytes('Content-Length: {}\r\n\r\n'.format(len(params_formatted)))
-        header += bytes(params_formatted + '\r\n')
+        header = 'POST {} HTTP/1.1\r\n'.format(url_parsed.path)
+        header += 'Host: {}\r\n'.format(url_parsed.netloc)
+        header += 'Content-Type: application/x-www-form-urlencoded\r\n'
+        header += 'Content-Length: {}\r\n\r\n'.format(len(params_formatted))
+        header += params_formatted + '\r\n'
 
         self.s.sendall(header)
-        print(self.s.recv(2048))
-        #print(header)
-    def test():
-        return "test"
+        return self.s.recv(2048)
+        
+    def get(self, url, params={}):
+        url_parsed = urlparse(url)
+        params_formatted = urllib.urlencode(params)
+        if len(params) != 0:
+            data = '?' + params_formatted
+        else:
+            data = ''
+        header = 'GET {} HTTP/1.1\r\n'.format((url_parsed.path) + (data))
+        header += 'Host: {}:{}\r\n'.format(self.HOST, self.PORT)
+        header += '\r\n'
+        self.s.sendall(header)
+        print(header)
+        return self.s.recv(2048)
+    def close(self):
+        self.s.shutdown(socket.SHUT_RDWR)
+        self.s.close()
--- test.py
+++ test.py
@@ -1,6 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 from http_sockets import *
 
 soc = sock("csec380-core.csec.rit.edu", 82)
-soc.post('http://csec380-core.csec.rit.edu:82/getSecure')
+tok = soc.post('http://csec380-core.csec.rit.edu:82/getSecure')
+tok = tok.split()[-1].strip('"')
+print(tok)
 
+capt = soc.post('http://csec380-core.csec.rit.edu:82/getFlag3Challenge', {'token' : tok })
+capt = capt.split()[-1].strip('"')
+print(capt)
+sol = eval(capt)
+flag = soc.post('http://csec380-core.csec.rit.edu:82/getFlag3Challenge', {'token' : tok, 'solution' : sol })
+#print(flag)
+soc.close()
+
+
+soc2 = sock("csec380-core.csec.rit.edu", 82)
+get = soc2.get('http://csec380-core.csec.rit.edu/')
+print(get)
+soc2.close()
